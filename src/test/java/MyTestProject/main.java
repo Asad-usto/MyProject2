428bd14f3ceeec328c401e1c9425211187516c85
Asad Ali Mumtaz <asad.mumtaz@10pearls.com>
2022-04-24 16:00:51 +0500

    Adding the changes in Project

--- src/test/java/MyTestProject/main.java
+++ src/test/java/MyTestProject/main.java
@@ -9,13 +9,14 @@ public class main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/");
-        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.xpath("//*[@id='content']/div/div[1]/h1")).getText();
+        driver.get("http://localhost:8085/");
+        driver.findElement(By.id("j_username")).sendKeys("AsadMumtaz");
+        driver.findElement(By.name("j_password")).sendKeys("10pearls1+");
+        driver.findElement(By.name("Submit")).click();
+        String text = driver.findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).getText();
         if(text.contains("Dashboard"));
         System.out.println("Logged In Successfully");
-        driver.quit();
+        if(driver.findElement(By.xpath("//*[@id='projectstatus']/tbody")).equals("Myproject3"));
+        driver.findElement(By.id("job_Myproject3")).click();
     }
 }
